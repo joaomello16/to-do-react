981f399d9605582bf6ad2e085cc8bf799a017ab8
João Pedro Vieira De Mello <joaomello1618@gmail.com>
2025-07-07 22:41:28 -0300

    lista funcionando

--- vite-project/src/components/Todolist.jsx
+++ vite-project/src/components/Todolist.jsx
@@ -1,23 +1,55 @@
 import Botao from "./Botao"
-function Todolist (){
+import { useState } from "react"
+function Todolist() {
 
+  const [tarefas, setTarefas] = useState([])
+  const [tarefa, setTarefa] = useState('')
 
-    return(
-         <header className='
+  function add() {
+    setTarefas([...tarefas, tarefa])
+    setTarefa('')
+    console.log(tarefas)
+  }
+  function mudaEstado(e) {
+    setTarefa(e.target.value)
+  }
+  return (
+    <div>
+    <header className='
     flex flex-col items-center
-    bg-amber-200 p-6 w-4/6 mx-auto mt-10 rounded border border-black'
+    bg-white p-6 w-4/6 mx-auto mt-10 rounded border border-black
+    shadow-gray-500 shadow-md'
     >
       <h1 className="p-4 text-5xl">To-do-list</h1>
       <div className="flex w-full">
         <input className='border border-black outline-none
-        rounded w-4/5 p-2 mr-2.5' 
-        
-        ></input>
-        <Botao>Adicionar</Botao>
-      
+        rounded w-4/5 p-2 mr-2.5'
+          type="text"
+          value={tarefa}
+          onChange={mudaEstado}
+          
+          
+        />
+        <Botao 
+        onClick={add}
+        >Adicionar</Botao>
+
       </div>
+
     </header>
-    )
+       <ul className="mt-6 w-4/6 m-auto ">
+        {tarefas.map(function (item, index) {
+          return (
+            <li key={index} className="bg-white border shadow-gray-500 shadow-md border-black p-2 my-2 rounded">
+              {item}
+            </li>
+          )
+        })}
+      </ul>
+
+    </div>
+    
+  )
 
 }
 export default Todolist
